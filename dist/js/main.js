f1a48ce39a3d74a19f872fc9af2818dfdaa23ffa
Alex Newman <Alex.newman@bedegaming.com>
2015-06-15 00:22:58 +0100

    ugly sticky header added

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,23 +2,43 @@
 
 $(function() {
 	window.onscroll = scroll;
+	var header = document.getElementsByClassName('header')[0];
+	var isHeaderVisible = false;
 
 	function scroll() {
-		var header = document.getElementsByClassName('header')[0];
-		var headerBackground = document.getElementsByClassName('background')[0];
 		var logo = document.getElementsByClassName('logo')[0];
 		var author = document.getElementsByTagName('h2')[0];
+		var title = document.getElementsByTagName('h1')[0];
 
-		if (window.pageYOffset < (header.clientHeight )) {
+
+		if (window.pageYOffset < header.clientHeight) {
 			logo.style.transform = 'translateY(-' + (window.pageYOffset / 2) + 'px)';
-			headerBackground.style.transform = 'translateY(' + (window.pageYOffset * 1.2) + 'px)';
-			author.style.transform = 'translateY(' + (window.pageYOffset * 0.7) + 'px)';
-
 		}
-		if (window.pageYOffset < 70) {
+
+		if (window.pageYOffset < 90) {
+			author.style.transform = 'translateY(' + (window.pageYOffset * 0.7) + 'px)';
+			title.style.transform = 'translateY(' + (window.pageYOffset * 0.7) + 'px)';
+
 			if (parseFloat(getComputedStyle(author).opacity) > -0.1) {
 				author.style.opacity = (1 - window.pageYOffset / 70).toFixed(1);
 			}
+		}
+
+		if (window.pageYOffset >= header.clientHeight && !isHeaderVisible) {
+			stickyHeader('show');
+		} else if (window.pageYOffset <= header.clientHeight && isHeaderVisible) {
+			stickyHeader('hide');
+		}
+	}
+
+	function stickyHeader(state) {
+		var sticky = document.getElementsByClassName('sticky')[0];
+		isHeaderVisible = !isHeaderVisible;
+
+		if (state === 'show') {
+			sticky.classList.add('show');
+		} else {
+			sticky.classList.remove('show');
 		}
 	}
 });
